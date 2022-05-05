6f00b33b31856662c84903734b735ac2fc386dd2
Catherine Kawara <ndutakawara2@gmail.com>
2022-05-05 15:19:08 +0300

    add about  section

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+function About({image, about}){
+    return(
+        <aside>
+            <img src={image} alt="blog logo"></img>
+            <p>{about}</p>
+        </aside>
+    )
+}
+export default About
